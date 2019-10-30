2b2a653a49b6cf2b7cea00e90071218cce32316f
Guido Wagner <wagner.guido.5@gmail.com>
2019-10-30 19:50:00 -0300

    Styles + fonts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import '@fortawesome/fontawesome-free/css/all.min.css'
+// import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css'
 
 import './index.css'
 import App from './App'
